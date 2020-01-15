c4e84246432265a00e7a82187fe88aeda3336981
”Nobu” <irurere@gmail.com>
2020-01-15 13:00:11 +0000

    kadai 1~4 sakusei

---
+++ 01.php
@@ -0,0 +1,7 @@
+<?php
+    $name="Nobu";
+    if($name=="Nobu"){
+        echo "私は ".$name."　です";
+    }else{
+        echo $name."ではありません";
+    }
---
+++ 03.php
@@ -0,0 +1,11 @@
+<?php
+    $fruits=[
+        "いちご",
+        "桃",
+        "なし",
+        "みかん",
+        "りんご",
+        ];
+        foreach($fruits as $i){
+            echo $i."\n";
+        }
